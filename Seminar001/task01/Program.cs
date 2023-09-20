cbdcd4aaf04c4271ee070dc3289fa6c716dc86e1
Dmitry <dmitry.wu@gmail.com>
2023-09-21 02:19:38 +0300

    add task01

--- Seminar001/task01/Program.cs
+++ Seminar001/task01/Program.cs
@@ -1,2 +1,13 @@
 ﻿// Коментарий, для примера работы 
 // весь текс ctrl+/ в нижнем ряду около шифта, выделяя весь текст
+
+// using System.ComponentModel;
+
+Console.Write("Input number: ");
+
+string numberString = Console.ReadLine()!;
+
+int numberInt = Convert.ToInt32(numberString);
+int result = numberInt * numberInt;
+
+Console.WriteLine(result);
