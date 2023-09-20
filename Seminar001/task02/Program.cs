6191d7db462a3c71e274e7e3a22580a093f3a174
Dmitry <dmitry.wu@gmail.com>
2023-09-21 02:34:41 +0300

    add task02

---
+++ Seminar001/task02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input 1s number");
+int number1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Input 2s number");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1==number2*number2) 
+{
+    Console.WriteLine("Yes!");
+}
+else
+{
+    Console.WriteLine("No :(");
+}
