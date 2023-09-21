37e33b60b284e5cbb7bea92c6488f8e6808890c3
Dmitry <dmitry.wu@gmail.com>
2023-09-22 01:43:41 +0300

    add tsk04

--- Seminar001/task03/Program.cs
+++ Seminar001/task03/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.WriteLine("Enter week number: ");
+
+int numberWeek = int.Parse(Console.ReadLine());
+
+if (numberWeek == 1) Console.WriteLine("It's: Понедельник");
+if (numberWeek == 2) Console.WriteLine("It's: Вторник");
+if (numberWeek == 3) Console.WriteLine("It's: Среда");
+if (numberWeek == 4) Console.WriteLine("It's: Четверг");
+if (numberWeek == 5) Console.WriteLine("It's: Пятница");
+if (numberWeek == 6) Console.WriteLine("It's: Суббота");
+if (numberWeek == 7) Console.WriteLine("It's: Воскресенье");
---
+++ Seminar001/task04/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter a number");
+int number = int.Parse(Console.ReadLine);
+
+int negative = -number;
+
+while (number <= negative)
+{
+    Console.WriteLine("number" +);
+    negative++;
+}
