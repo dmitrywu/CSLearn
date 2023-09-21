24770b633d35ba3d8abd4bbb4cc979ff8f02d927
Dmitry <dmitry.wu@gmail.com>
2023-09-22 02:39:09 +0300

    add hw01 solution

--- Seminar001/HomeWork 01/Program.cs
+++ Seminar001/HomeWork 01/Program.cs
@@ -2,3 +2,25 @@
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
+
+Console.Write("Enter 1st number: ");
+int number1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter 2st number: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1==number2)
+{
+    Console.WriteLine("Числа равны");
+}
+
+else if (number1 > number2)
+{
+    Console.WriteLine(number1 + " Больше чем " + number2);
+}
+else
+{
+    Console.WriteLine(number2 + " Больше чем " + number1);
+}
+
+
