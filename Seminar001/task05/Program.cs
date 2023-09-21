1239bc51b23351f496184eb29d882ff48eb763e4
Dmitry <dmitry.wu@gmail.com>
2023-09-22 02:14:29 +0300

    add task05

---
+++ Seminar001/task05/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a number");
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 100 && number < 1000)
+{
+    Console.WriteLine("Yes, result is: " + number % 10);
+}
+else
+{
+    Console.WriteLine("wrong number");
+}
