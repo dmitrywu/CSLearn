59d193ed4449e153cf88d70b5b7a7245f8614a0d
Dmitry <dmitry.wu@gmail.com>
2023-09-24 18:09:59 +0300

    add task06

--- Seminar002/task06/Program.cs
+++ Seminar002/task06/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = Random.Shared.Next(10, 100);
+int firstDigit = number / 10;
+int lastDigit = number % 10;
+
+Console.Clear();
+
+Console.WriteLine("Сгенерированное число: " + number);
+
+if (firstDigit > lastDigit)
+{
+    System.Console.WriteLine("Первая цифра больше второй: " + firstDigit + " > " + lastDigit);
+}
+else if (firstDigit < lastDigit)
+{
+    System.Console.WriteLine("Вторая цифра больше первой: " + lastDigit + " > " + firstDigit);
+}
