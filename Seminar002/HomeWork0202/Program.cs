6e7026e1a32893d1f82989ab21b2ab1239c9e4ed
Dmitry <dmitry.wu@gmail.com>
2023-09-26 02:44:06 +0300

    add hw0202

--- Seminar002/HomeWork0202/Program.cs
+++ Seminar002/HomeWork0202/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+Console.Write("Введите число (100 или больше): ");
+int number = int.Parse(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет. Число меньше 100");
+}
+else
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    
+    Console.Write($"Третья цифра: {number%10}");
+}
