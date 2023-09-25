f43eae84d95ea950d2a08a3a00dcce85c86a7ee4
Dmitry <dmitry.wu@gmail.com>
2023-09-26 02:26:40 +0300

    add hw0201 & hw0203

---
+++ Seminar002/HomeWork0201/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите число:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine($"Введённое число: {number}");
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Число должно быть в диапазоне от 100 до 999");
+
+}
+else 
+{
+    int res = (number/10)%10;
+    Console.WriteLine($"Ok, вторая цифра: {res}");
+}
+
---
+++ Seminar002/HomeWork0203/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+Console.Write("Введите номер дня недели (1-7): ");
+int number = int.Parse(Console.ReadLine());
+//,,..Console.WriteLine($"Введённое число: {number}");
+
+if (number <1 || number >7)
+{
+    Console.WriteLine("Не подходит, введите цифру от 1 до 7");
+}
+else 
+{
+    if (number == 6 || number == 7)
+    {
+        Console.WriteLine("Выходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий день");
+    }
+}
