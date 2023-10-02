651a615bd9f550166648cf28950dbae1485556ce
Dmitry <dmitry.wu@gmail.com>
2023-10-03 02:55:32 +0300

    add all s3 hw (hw02 - formula

--- Seminar003/homework0302_21/Program.cs
+++ Seminar003/homework0302_21/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+//Math.Sqrt() – математическая функция которая извлекает квадратный корень. В аргументных скобках указывается значение числа, из которого нужно извлечь квадратный корень.
+//Math.Sqrt(x)
+//Math.Pow() – возведения числа в степень. В аргументных скобках через запятую указываются два аргумента (1-ый аргумент - число, которое возводим в степень, 2-ой – степень, в которую возводим число).
+//Math.Pow(x, y)
+//result = Math.Sqrt((x2-x1),2)
+
+int xFin = Math.Pow((x2-x1),2);
+int yFin = Math.Pow((y2-y1),2);
+int zFin = Math.Pow((z2-z1),2);
+int result = Math.Sqrt(xFin+yFin+zFin);
+
--- Seminar003/homework0303_23/Program.cs
+++ Seminar003/homework0303_23/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//tаблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+Console.Clear();
+
+Console.WriteLine("Введите число (больше или равно 1): ");
+int number = int.Parse(Console.ReadLine());
+int idx = 1;
+int NumPow;
+
+if (number<1)
+{
+    Console.WriteLine("Число должно быть больше или равно 1");
+}
+else if (number == 1)
+{
+    Console.WriteLine("1. и всё.");
+}
+else 
+{
+    for (idx = 1; idx<=number; idx++)
+    {
+        NumPow = idx*idx;
+        Console.Write($"{idx}^2 = {NumPow}   ");
+    }
+}
