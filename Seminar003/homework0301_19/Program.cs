c1c2509c8d121e1c5ace81858640be6675d19584
Dmitry <dmitry.wu@gmail.com>
2023-10-03 02:18:27 +0300

    add hw030119

--- Seminar003/homework0301_19/Program.cs
+++ Seminar003/homework0301_19/Program.cs
@@ -1,8 +1,22 @@
-﻿Clear.Console();
+﻿Console.Clear();
 
-Console.WriteLine("Введите gятизначное число: ");
+Console.WriteLine("Введите пятизначное число: ");
 int number = int.Parse(Console.ReadLine());
+int arg1 = number/10000;
+int arg2 = (number/1000)%10;
+int arg4 = (number%100)/10;
+int arg5 = number%10;
+Console.WriteLine($"{arg1} {arg2} {arg4} {arg5}");
 
 if ( number < 10000 || number > 99999)
 {
+    Console.WriteLine("Число не пятизначное");
+}
+else if (arg1==arg5 && arg2==arg4)
+{
+    Console.WriteLine("True (палиндром)");
+}
+else
+{
+    Console.WriteLine("False (НЕ палиндром)");
 }
---
+++ Seminar003/test030119/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Answer
+{
+    static bool IsPalindrome(int number){
+      // Введите свое решение ниже
+      Console.WriteLine("Введите пятизначное число: ");
+      int numb = int.Parse(Console.ReadLine());
+      
+      if (numb < 10000 || numb >99999)
+      {
+        Console.WriteLine("Число не пятизначное");
+        Console.WriteLine("False");
+      }
+      
+    }
+  
+  // Не удаляйте и не меняйте метод Main! 
+      static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 64546;
+        }
+
+        // Не удаляйте строки ниже
+        bool result = IsPalindrome(number);
+        System.Console.WriteLine($"{result}");
+    }
+}
